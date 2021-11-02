80910442ac741036292c4308636a3122e5196934
Trịnh Hoàng Long <trinhhoanglong200@gmail.com>
2021-11-02 22:41:56 +0700

    make it better

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/classes',classesRoutes);
-
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
 const CONNECTION_URL= `mongodb+srv://longdien0098:longdien0098@cluster0.2uaet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT =process.env.PORT || 5000;
 
