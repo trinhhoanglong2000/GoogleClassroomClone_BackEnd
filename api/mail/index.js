9bd64e752a52a3f2848428cc93c27f2250c620b6
Trần Phi Long <50954310+goku2000vn@users.noreply.github.com>
2021-11-09 23:40:20 +0700

    Send Mail - Demo
    Demo send mail API to email

--- api/mail/index.js
+++ api/mail/index.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const mailController = require('./mailController');
+const nodemailer =  require('nodemailer');
 
-/* GET users listing. */
-router.get('/', mailController.senndmail());
+router.get('/send', function(req, res) {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: 'triplelacademic@gmail.com', // generated ethereal user
+          pass: '10DiemDoAn', // generated ethereal password
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false
+        }
+      });
+    
+      var mainOptions = {
+        from: '"TripleLAcademic" <triplelacademic@gmail.com>', // sender address
+        to: "longbinkg@gmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+      }
+      // send mail with defined transport object
+    transporter.sendMail(mainOptions, function(err){
+        if (err) {
+            console.log(err);
+            res.json('Lỗi');
+        } else {
+            res.json('Thành công');
+        }
+    });
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const classesRouter = require('./api/classes');
+const mailRouter = require('./api/mail');
 const app = express();
 
 // view engine setup
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/classes', classesRouter);
+app.use('/mail', mailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
